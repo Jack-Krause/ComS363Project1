8d500a9908088bc3d149c823a8a20fa65253002a
Jack Krause <jmkrause@iastate.edu>
2025-02-17 18:38:18 -0600

    fixes and relationship partitipation

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,5 +1,24 @@
 create database University;
 
+drop table if exists register;
+drop table if exists offers;
+drop table if exists administer;
+drop table if exists minor;
+drop table if exists major;
+drop table if exists courses;
+drop table if exists degrees;
+drop table if exists students;
+drop table if exists department;
+
+create table department (
+    name varchar(10) not null,
+    code int not null,
+    phone int,
+    college varchar(20),
+    primary key (code),
+    unique (name)
+);
+
 create table students (
     p_phone int,
     p_addr varchar(50),
@@ -8,30 +27,22 @@ create table students (
     dob date,
     gender char(1),
     name varchar(20),
-    snum int,
+    snum int not null,
     ssn int,
     primary key (snum)
 );
 
 create table courses (
     name varchar(20),
-    number int,
+    number int not null,
     description varchar(50),
     credit_hours int,
     level int,
     primary key (number)
 );
 
-create table departments (
-    name varchar(50),
-    code int,
-    phone int,
-    college varchar(20),
-    primary key (code)
-);
-
 create table degrees (
-    name varchar(50),
+    name varchar(50) not null,
     level int,
     primary key (name)
 );
@@ -40,37 +51,38 @@ create table degrees (
 create table register (
     regtime date,
     grade int,
-    snum int,
-    number int,
-    -- primary key (snum, number),
+    snum int not null,
+    number int not null,
+    primary key (snum, number),
     foreign key (snum) references students (snum),
     foreign key (number) references courses (number)
 );
 
 create table offers (
-    number int,
-    code int,
+    number int not null,
+    code int not null,
+    primary key (number),
     foreign key (number) references courses (number),
-    foreign key (code) references departments (code)
+    foreign key (code) references department (code)
 );
 
 create table administer (
-    code int,
-    name varchar(50),
+    code int not null,
+    name varchar(50) not null,
     foreign key (code) references department (code),
     foreign key (name) references degrees
 );
 
 create table minor (
-    snum int,
-    name varchar(50),
+    snum int not null,
+    name varchar(50) not null,
     foreign key (snum) references students (snum),
     foreign key (name) references degrees (name)
 );
 
 create table major (
-    snum int,
-    name varchar(50),
+    snum int not null,
+    name varchar(50) not null,
     foreign key (snum) references students (snum),
     foreign key (name) references degrees (name)
 );
