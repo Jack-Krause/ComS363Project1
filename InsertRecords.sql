8a4415bb76214e8f5b7279129c78a29fc51f1681
Jack Krause <jmkrause@iastate.edu>
2025-02-24 21:40:31 -0600

    load data from depts

--- InsertRecords.sql
+++ InsertRecords.sql
@@ -1,10 +1,5 @@
 SET GLOBAL local_infile=1;
 
-create table students (
-    sid int,
-    ssn int,
-    primary key (ssn)
-);
 
 load data local infile 'C:\\Users\\jackm\\OneDrive\\CS3630\\Project1\\students.csv'
 into table students
@@ -12,4 +7,17 @@ fields terminated by ','
 enclosed by '"' 
 lines terminated by '\r\n'
 ignore 1 rows
-(sid, ssn, @col3, @col4, @col5, @col6, @col7, @col8, @col9);
+(sid, ssn, name, gender, dob, c_addr, c_phone, p_addr, p_phone);
+-- (sid, ssn, @col3, @col4, @col5, @col6, @col7, @col8, @col9);
+
+
+load data local infile 'C:\\Users\\jackm\\OneDrive\\CS3630\\Project1\\departments.csv'
+into table departments
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\r\n'
+ignore 1 rows
+(dcode, dname, phone, college);
+
+select * from departments;
+select * from students;
