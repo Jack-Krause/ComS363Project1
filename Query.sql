b8d84fc3e79a9505ae51b16acd4ce0d54cb3b9ad
Jack Krause <jmkrause@iastate.edu>
2025-02-28 00:33:09 -0600

    queries added

--- Query.sql
+++ Query.sql
@@ -6,10 +6,20 @@ where name = "Gail";
 select m.name, m.level
 from major m
 join students s on s.sid = m.sid
-where s.name = "Julie";
+where s.name = 'Julie';
 
+-- query 3
+select c.cname, c.cnumber
+from courses c
+join offers o on o.cnumber = c.cnumber
+join departments d on o.dcode = d.dcode
+where d.dname = 'Computer Science'
+order by c.cnumber asc;
 
-
-
+-- query 4
+select s.name
+from students s
+join register r on r.sid = s.sid
+where r.regtime = 'Fall2022';
 
 
