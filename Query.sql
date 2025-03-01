cbbb87c153b6ecca4cf697a5e78a7de7c2a0bf0e
Jack Krause <jmkrause@iastate.edu>
2025-02-28 18:18:16 -0600

    last query added

--- Query.sql
+++ Query.sql
@@ -77,6 +77,51 @@ join (
 where s.gender = 'F';
 
 -- query 12:
+select name, level, count(*) as num_students
+from major
+group by name, level
+having count(*) = 
+(
+    select count(*)
+    from major
+    group by name, level
+    order by count(*) desc
+    limit 1
+)
+order by name;
+    
+-- query 13:
+select name, level, count(*) as num_students
+from 
+(
+    select sid, name, level from major
+    union
+    select sid, name, level from minor
+) as degrees
+group by name, level
+having count(*) = 
+(
+    select count(*)
+    from (
+        select sid, name, level from major
+        union
+        select sid, name, level from minor
+    ) as degreescount
+    group by name, level
+    order by count(*) desc
+    limit 1
+)
+order by name;
+    
+    
+
+
+
+
+
+
+
+
 
 
 
