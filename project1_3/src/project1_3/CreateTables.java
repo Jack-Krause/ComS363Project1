b06d535d4cc346c7bb0240c0f5b54e90299670e4
Jack Krause <jmkrause@iastate.edu>
2025-03-22 20:08:06 -0500

    added create tables

--- project1_3/src/project1_3/CreateTables.java
+++ project1_3/src/project1_3/CreateTables.java
@@ -14,7 +14,8 @@ public class CreateTables {
 			//Set up connection
 			connect = DriverManager.getConnection(dbServer,userName,password);
 		} catch(Exception e) {
-
+			System.err.println("error connecting");
+			e.printStackTrace();
 		}
 		
 	
@@ -35,8 +36,9 @@ public class CreateTables {
 			
 			stmt.executeBatch();
 			stmt.clearBatch();
-			System.out.println("dropped tables");
+			System.out.println("Dropped tables");
 		} catch (SQLException e) {
+			System.err.println("Error clearing tables");
 			e.printStackTrace();
 		}
 		try {
@@ -61,20 +63,97 @@ public class CreateTables {
 					"	sid integer not null,\r\n" + 
 					"	ssn integer not null,\r\n" + 
 					"	primary key (ssn),\r\n" + 
-					"	unique (sid),\r\n" + 
+					"	unique (sid)\r\n" + 
 					");";
 			
+			String create_courses = "CREATE TABLE courses (\r\n" +
+					"	cname varchar(50),\n\n" +
+    				"	cnumber integer not null,\r\n" +
+    				"	description varchar(50),\r\n" +
+    				"	credithours integer,\r\n" +
+    				"	level varchar(20),\r\n" +
+    				"	department_code integer,\r\n" +
+    				"	primary key (cnumber),\r\n" +
+    				"	foreign key (department_code) references departments(dcode)\r\n" +
+    				");";
 			
+			String create_degrees = 
+					"""
+					create table degrees (\r\n
+						    dgname varchar(50) not null,\r\n
+						    level varchar(5) not null,\r\n
+						    department_code integer,\r\n
+						    primary key (dgname, level),\r\n
+						    foreign key (department_code) references departments(dcode)\r\n
+						);
+					""";
+			
+			String create_register = 
+					"""
+					create table register (\r\n
+						sid integer not null,\r\n
+						course_number integer not null,\r\n
+						regtime varchar(20),\r\n
+						grade integer,\r\n
+						primary key (sid, course_number),\r\n
+						foreign key (sid) references students(sid),\r\n
+						foreign key (course_number) references courses(cnumber)\r\n
+					);
+					""";
+			
+			
+			String create_offers = 
+					"""
+					create table offers (
+						cnumber integer not null,\r\n
+						dcode integer not null,\r\n
+						primary key (cnumber),\r\n
+						foreign key (cnumber) references courses(cnumber),\r\n
+						foreign key (dcode) references departments(dcode)\r\n
+					);
+					""";
+			
+			String create_administer = 
+					"""
+					create table administer (\r\n
+						dcode integer not null,\r\n
+						dgname varchar(50) not null,\r\n
+						foreign key (dcode) references departments(dcode),\r\n
+						foreign key (dgname) references degrees(dgname)\r\n
+					);
+					""";
+			
+			String create_minor = 
+					"""
+					create table minor (\r\n
+						sid integer not null,\r\n
+						name varchar(50) not null,\r\n
+						level varchar(5) not null,\r\n
+						primary key (sid, name, level),\r\n
+						foreign key (sid) references students(sid),\r\n
+						foreign key (name, level) references degrees(dgname, level)\r\n
+					);
+					""";
+    			
 			// To update data in a database, call the executeUpdate(String SQL) method
-			stmt.executeUpdate(create_department);	
+//			stmt.executeUpdate(create_department);	
 				
-//			stmt.addBatch(create_student);
-//			stmt.executeBatch();
+			stmt.addBatch(create_student);
+			stmt.addBatch(create_department);
+			stmt.addBatch(create_courses);
+			stmt.addBatch(create_degrees);
+			stmt.addBatch(create_register);
+			stmt.addBatch(create_offers);
+			stmt.addBatch(create_administer);
+			stmt.addBatch(create_minor);
 			
-			System.out.println("Created students table");  
+			stmt.executeBatch();
+			stmt.clearBatch();
+			
+			System.out.println("Created tables");  
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.err.println("Error creating tables");
 			e.printStackTrace();
 		}
 		
